2034d3acace4609c5d54283089d8c21a599617e8
Yaochenger <1516081466@qq.com>
2022-12-15 14:12:37 +0800

    add SDKConfig.h

---
+++ CLI/cli_test/include/SDKConfig.h
@@ -0,0 +1,12 @@
+/*
+ * appconfig.h
+ *
+ *  Created on: Feb 27, 2021
+ *      Author: qlblue
+ */
+
+
+#define	UART_ID_CONSOLE	0	// UART_ID to use as console
+#define DEBUG_UART		0	// UART_ID to use for dbg_uart()
+
+
